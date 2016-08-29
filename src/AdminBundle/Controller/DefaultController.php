427ce87a51248a74f49a164d65d0190da204cfe7
Francois <maps_red@yahoo.fr>
2016-08-30 01:08:03 +0200

    Add AdminBundle

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -2,16 +2,45 @@
 
 namespace AdminBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Image;
+use AppBundle\Entity\Project;
+use AppBundle\Form\ProjectType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/form", name="formPage")
+     * @param Request $request
+     * @return Response
      */
-    public function indexAction()
+    public function formAction(Request $request)
     {
-        return $this->render('AdminBundle:Default:index.html.twig');
+        $form = $this->createForm(ProjectType::class);
+        $form->handleRequest($request);
+        $formView = $form->createView();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Project $project */
+            $project = $form->getData();
+            for ($i = 0; $i < 4; $i++) {
+                /** @var Image $image */
+                $image = $form->get("image_$i")->getData();
+                if ($image && $image->getPath()) {
+                    $project->addImage($image);
+                }
+            }
+
+            $this->getDoctrine()->getManager()->persist($project);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirect($this->generateUrl('formPage'));
+        }
+
+        return $this->render('AppBundle:Default:form.html.twig', ["form" => $formView]);
     }
 }
